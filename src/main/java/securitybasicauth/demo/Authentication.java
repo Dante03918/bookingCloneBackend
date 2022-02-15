50422264ff1e7c11588495a50b3c1e874c69b204
Dante <dante03918@gmial.com>
2022-02-15 22:19:57 +0100

    signUp and logIn methods was moved to the separate class (Authentication class)

--- src/main/java/securitybasicauth/demo/Authentication.java
+++ src/main/java/securitybasicauth/demo/Authentication.java
@@ -1,22 +1,54 @@
 package securitybasicauth.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 import securitybasicauth.demo.models.LoginUserModel;
+import securitybasicauth.demo.models.RegisterUserModel;
+import securitybasicauth.demo.repositories.RegisterUserRepo;
+import securitybasicauth.demo.utils.JwtTokenUtils;
+import securitybasicauth.demo.utils.RegisterModelValidation;
 
 @Component
 public class Authentication {
 
+    private final RegisterUserRepo registerUserRepo;
+    private final Authentication authentication;
+    private final JwtTokenUtils jwtTokenUtils;
+    private final RegisterModelValidation registerModelValidation;
+    private PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
 
-    public Authentication(AuthenticationManager authenticationManager) {
+    public Authentication(RegisterUserRepo registerUserRepo,
+                          Authentication authentication,
+                          JwtTokenUtils jwtTokenUtils,
+                          RegisterModelValidation registerModelValidation,
+                          AuthenticationManager authenticationManager) {
+        this.registerUserRepo = registerUserRepo;
+        this.authentication = authentication;
+        this.jwtTokenUtils = jwtTokenUtils;
+        this.registerModelValidation = registerModelValidation;
         this.authenticationManager = authenticationManager;
     }
 
+    @Bean
+    PasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {      // passwordEncoder injected with constructor causes circular dependency exception
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public boolean authenticate(LoginUserModel loginUserModel) throws BadCredentialsException {
 
@@ -24,7 +56,33 @@ public class Authentication {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUserModel.getEmail(), loginUserModel.getPassword()));
             return true;
         } catch (BadCredentialsException badCredentialsException) {
-            throw new ResponseStatusException( HttpStatus.UNAUTHORIZED, "Incorrect Credentials");
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Incorrect Credentials");
+        }
+    }
+
+    public ResponseEntity<?> signUp(RegisterUserModel registerUserModel) {
+
+        boolean exists = registerUserRepo.existsByEmail(registerUserModel.getEmail());
+
+        if (!registerModelValidation.validModel(registerUserModel)) {
+            throw new ResponseStatusException(HttpStatus.PARTIAL_CONTENT, "All form fields must be filled");
+        } else {
+            if (!exists) {
+                registerUserModel.setPassword(passwordEncoder.encode(registerUserModel.getPassword()));
+                registerUserRepo.save(registerUserModel);
+                return new ResponseEntity<>("User created", HttpStatus.OK);
+            } else {
+                throw new ResponseStatusException(HttpStatus.FOUND, "User already exists");
+            }
+        }
+    }
+
+    public ResponseEntity<?>logIn(LoginUserModel loginUserModel) {
+
+        if (authentication.authenticate(loginUserModel)) {
+            return ResponseEntity.ok(jwtTokenUtils.generateToken(loginUserModel.getEmail()));
+        } else {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Username or password is not correct");
         }
     }
 }
--- src/main/java/securitybasicauth/demo/controllers/AuthenticationController.java
+++ src/main/java/securitybasicauth/demo/controllers/AuthenticationController.java
@@ -1,76 +1,31 @@
 package securitybasicauth.demo.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import securitybasicauth.demo.utils.JwtTokenUtils;
 import securitybasicauth.demo.models.LoginUserModel;
 import securitybasicauth.demo.models.RegisterUserModel;
-import securitybasicauth.demo.repositories.RegisterUserRepo;
-import securitybasicauth.demo.utils.RegisterModelValidation;
 import securitybasicauth.demo.Authentication;
 
 @CrossOrigin(origins = "/**", maxAge = 3600)
 @RestController
 public class AuthenticationController {
 
-
-    private final RegisterUserRepo registerUserRepo;
     private final Authentication authentication;
-    private final JwtTokenUtils jwtTokenUtils;
-    private final RegisterModelValidation registerModelValidation;
-    private PasswordEncoder passwordEncoder;
 
-    public AuthenticationController(RegisterUserRepo registerUserRepo,
-                                    Authentication authentication,
-                                    JwtTokenUtils jwtTokenUtils,
-                                    RegisterModelValidation registerModelValidation) {
-        this.registerUserRepo = registerUserRepo;
+    public AuthenticationController(Authentication authentication){
         this.authentication = authentication;
-        this.jwtTokenUtils = jwtTokenUtils;
-        this.registerModelValidation = registerModelValidation;
-    }
-
-    @Autowired
-    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {      // passwordEncoder injected with constructor causes circular dependency exception
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @Bean
-    PasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> logIn(@RequestBody LoginUserModel loginUserModel) {
+    public ResponseEntity<?>logIn(@RequestBody LoginUserModel loginUserModel) {
 
-        if (authentication.authenticate(loginUserModel)) {
-            return ResponseEntity.ok(jwtTokenUtils.generateToken(loginUserModel.getEmail()));
-        } else {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Username or password is not correct");
-        }
+        return authentication.logIn(loginUserModel);
     }
 
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody RegisterUserModel registerUserModel) {
 
-        boolean exists = registerUserRepo.existsByEmail(registerUserModel.getEmail());
+        return authentication.signUp(registerUserModel);
 
-        if (!registerModelValidation.validModel(registerUserModel)) {
-            throw new ResponseStatusException(HttpStatus.PARTIAL_CONTENT, "All form fields must be filled");
-        } else {
-            if (!exists) {
-                registerUserModel.setPassword(passwordEncoder.encode(registerUserModel.getPassword()));
-                registerUserRepo.save(registerUserModel);
-                return new ResponseEntity<>( "User created", HttpStatus.OK);
-            } else {
-                throw  new ResponseStatusException(HttpStatus.FOUND, "User already exists");
-            }
-        }
     }
 }
